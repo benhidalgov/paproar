963f93007c388d892a8d537df04d2cb839f671ea
Benjamin Hidalgo <ben.hidalgov@duocuc.cl>
2024-11-08 01:28:11 -0300

    css

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,12 +1,12 @@
 import { Subscription } from 'rxjs';
 import { AuthService } from './../../services/auth.service2';
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
 import { IonFooter, IonToolbar, IonSegment, IonSegmentButton, IonIcon } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { homeOutline, pawOutline, pencilOutline, qrCodeOutline } from 'ionicons/icons';
+import { homeOutline, pawOutline, pencilOutline, qrCodeOutline, schoolOutline, gridOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-footer',
@@ -14,31 +14,39 @@ import { homeOutline, pawOutline, pencilOutline, qrCodeOutline } from 'ionicons/
   styleUrls: ['./footer.component.scss'],
   standalone: true,
   imports: [
-      CommonModule    // CGV-Permite usar directivas comunes de Angular
-    , FormsModule     // CGV-Permite usar formularios
-    , TranslateModule // CGV-Permite usar pipe 'translate'
-    , IonFooter, IonToolbar, IonSegment, IonSegmentButton, IonIcon
+    CommonModule,     // Allows usage of common Angular directives
+    FormsModule,      // Allows usage of forms
+    TranslateModule,  // Allows usage of translate pipe
+    IonFooter, IonToolbar, IonSegment, IonSegmentButton, IonIcon
   ]
 })
-export class FooterComponent {
-
+export class FooterComponent implements OnInit {
+  
   componenteSeleccionada: string = 'codigoqr';
   
-  cambiarComponente($event: any) {
-
-    this.componenteSeleccionada = $event.detail.value
-    this.authService.componenteSeleccionada.next(this.componenteSeleccionada)
-  }
-
-  selectedButton = 'welcome';
   @Output() footerClick = new EventEmitter<string>();
+  selectedButton = 'welcome';
 
   constructor(private authService: AuthService) {
-    addIcons({ homeOutline, qrCodeOutline, pawOutline, pencilOutline });
+    addIcons({ homeOutline, schoolOutline, pencilOutline, gridOutline, qrCodeOutline, pawOutline });
+  }
+
+  ngOnInit() {
+    // Initialize `componenteSeleccionada` from AuthService or use a default
+    this.authService.componenteSeleccionada.subscribe(value => {
+      this.componenteSeleccionada = value;
+    });
+
+    // Set initial component based on current page or navigation state if needed
+    this.authService.componenteSeleccionada.next(this.componenteSeleccionada);
+  }
+
+  cambiarComponente($event: any) {
+    this.componenteSeleccionada = $event.detail.value;
+    this.authService.componenteSeleccionada.next(this.componenteSeleccionada);
   }
 
   sendClickEvent($event: any) {
     this.footerClick.emit(this.selectedButton);
   }
-
 }
